bed52ad2982dfea1e5ce88231faf67954a37da85
Jack Fisher <jackfisher2012@gmail.com>
2018-04-04 11:48:11 +0100

    General working version using Futures

--- src/main/scala/com/lottery/ticket/LotteryTicket.scala
+++ src/main/scala/com/lottery/ticket/LotteryTicket.scala
@@ -1,11 +1,16 @@
 package com.lottery.ticket
 
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 //import scala.concurrent.Future
 //import scala.concurrent._
 
 object LotteryTicket {
   def main(args: Array[String]): Unit = {
-
+    generateFiveMillion()
+    Thread.sleep(50000)
   }
   //Function to generate one random number between 1 and 90
   def generateOneRandom: Int = (Math.floor(Math.random() * 90) + 1).toInt
@@ -14,19 +19,29 @@ object LotteryTicket {
   @annotation.tailrec
   def generateFive: Array[Int] = {
     val first = Array(generateOneRandom, generateOneRandom, generateOneRandom, generateOneRandom, generateOneRandom)
-    if (first.distinct.size == first.size) first
+    if (first.distinct.length == 5) first
     else generateFive
   }
 
   //Function to recursively generate 5 million tickets from previous function (maybe store as:
   //Nested array
   //JSON
-//  def generateFiveMillion = {
-//    val fiveMillion: IndexedSeq[Future[IndexedSeq[Array[Int]]]] = for (i <- 0 to 100) yield Future {
-//      for (i <- 1 to 50000) yield generateFive
-//    }
-//  }
-
+  def generateXTickets(x: Int): IndexedSeq[Future[Array[Int]]] = for(i <- 1 to x) yield Future {
+    generateFive
+  }
+  def generateFiveMillion(): Unit = {
+    println("-------------------- Please generate entropy by moving the mouse as much as possible ------------------")
+    val combine: Future[IndexedSeq[Array[Int]]] = Future.sequence(generateXTickets(5000000))
+    combine.onComplete {
+      case Success(r) => checkWinners(r)
+      case Failure(e) => println(e.printStackTrace())
+    }
+  }
   //Function to check against previous 5 million tickets to check for match against winning ticket (hash map)?
-
+  def checkWinners(tickets: IndexedSeq[Array[Int]], winner: Array[Int] = generateFive): Int = {
+    println("------------------------------------ Entropy generated, thanks! ---------------------------------------")
+    val winners = tickets.count(_ sameElements  winner)
+    println("Winners: " + winners)
+    winners
+  }
 }
--- src/test/scala/com/lottery/ticket/LotteryTicketTest.scala
+++ src/test/scala/com/lottery/ticket/LotteryTicketTest.scala
@@ -1,6 +1,7 @@
 package com.lottery.ticket
 
 import org.scalatest._
+import scala.concurrent.Future
 
 class LotteryTicketTest extends WordSpec with Matchers  {
 
@@ -29,5 +30,244 @@ class LotteryTicketTest extends WordSpec with Matchers  {
       overOdds.size should be <= 3
     }
   }
+  "checkWinners" should {
+    val testData = List(IndexedSeq(Array(1, 2, 3, 4, 5), Array(2, 3, 4, 5, 6)), IndexedSeq(Array(1, 2, 3, 4, 5), Array(3, 4, 5, 6, 7), Array(4, 5, 6, 7, 8)))
+    "return an Int given valid input" in {
+      for (ticketColl <- testData) {
+        LotteryTicket.checkWinners(ticketColl).isInstanceOf[Int] shouldBe true
+      }
+    }
+    "return 0 when there is no match" in {
+      testData.foreach { ticketColl =>
+        LotteryTicket.checkWinners(ticketColl, Array(90, 89, 88, 87, 86)) shouldBe 0
+      }
+    }
+    "return 1 when there is one match" in {
+      testData.foreach { ticketColl =>
+        LotteryTicket.checkWinners(IndexedSeq(Array(1, 2, 3, 4, 5), Array(2, 3, 4, 5, 6)), Array(1, 2, 3, 4, 5)) shouldBe 1
+      }
+    }
+    "return 200 when there are 200 matches" in {
+      val testData = IndexedSeq(Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5),
+        Array(1,2,3,4,5)
+      )
+      LotteryTicket.checkWinners(testData, Array(1,2,3,4,5)) shouldBe 200
+    }
+  }
+}
 
+class AsyncLotteryTicketTest extends AsyncWordSpec with Matchers {
+  "generateXTickets" should {
+    "eventually generate an IndexedSeq of Array[Int]" in {
+      val futureSeq = Future.sequence(LotteryTicket.generateXTickets(500))
+      futureSeq.map{seq => assert(seq.isInstanceOf[IndexedSeq[Array[Int]]])}
+    }
+    "eventually generate an IndexedSeq of length x where x is given as 500" in {
+      val futureSeq = Future.sequence(LotteryTicket.generateXTickets(500))
+      futureSeq.map{seq => assert(seq.length == 500)}
+    }
+    "eventually generate an IndexedSeq of length x where x is given as 5e6" in {
+      val futureSeq = Future.sequence(LotteryTicket.generateXTickets(5000000))
+      futureSeq.map{seq => assert(seq.length == 5000000)}
+    }
+    //Maybe add checks for randomness, although we do check randomness without futures above
+  }
 }
